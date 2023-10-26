f5e83065fd38c559218133477910c9abb5ab5ee8
NTienDuy <105479766+NTienDuy@users.noreply.github.com>
2023-10-26 16:44:30 +0700

    update_ui_project

--- app/(client)/du-an/page.tsx
+++ app/(client)/du-an/page.tsx
@@ -7,7 +7,6 @@ import { PiPhoneCallLight } from "react-icons/pi";
 import { AiOutlineHeart } from "react-icons/ai";
 
 const Project = () => {
-
   return (
     <div className="container max-w-5xl mx-auto m-24">
       <div className="md:flex flex-col md:flex-row">
@@ -54,190 +53,192 @@ const Project = () => {
               </div>
             </div>
           </div>
-
-          <div className="lg:max-w-xl">
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 2xl:grid-cols-2 border-2 ">
-              <img
-                src="https://file4.batdongsan.com.vn/crop/562x284/2023/09/28/20230928101136-0965_wm.jpg"
-                alt=""
-                className="w-full h-auto sm:h-full"
-              />
-              <div className="w-auto scroll-px-0.5 ml-1">
-                <div>
-                  <img
-                    src="https://file4.batdongsan.com.vn/crop/283x141/2023/09/28/20230928101136-ddd4_wm.jpg"
-                    alt=""
-                    className="w-full"
-                  />
-                </div>
-                <div className="flex pt-0.5">
-                  <img
-                    src="https://file4.batdongsan.com.vn/crop/140x140/2023/08/02/20230802142309-7dec_wm.jpg"
-                    alt=""
-                    className="w-full"
-                  />
-                  <img
-                    src="https://file4.batdongsan.com.vn/crop/140x140/2023/09/28/20230928101136-14c8_wm.jpg"
-                    alt=""
-                    className="pl-1"
-                  />
-                </div>
-              </div>
-            </div>
-            <div className="mx-auto border-1 p-4 shadow-inner">
-              <span className="font-bold text-sm md:text-xl">
-                BÁN CĂN STUDIO, 2 VÀ 3 PHÒNG NGỦ ĐẦY ĐỦ NỘI THẤT GIÁ TỐT NHẤT
-                TẠI VINHOMES TRẦN DUY HƯNG
-              </span>
-              <div className="flex flex-wrap gap-0 md:gap-6">
-                <span className="text-red-500 font-bold">4,3 tỷ</span>
-                <span className="text-red-500 font-bold">· 76 m²</span>
-                <span>· 56,58 tr/m²</span>
-                <span className="flex">
-                  · 2
-                  <span className="mt-1 mx-2">
-                    <LuBed />
-                  </span>
-                </span>
-                <span className="flex">
-                  · 2
-                  <span className="mt-1 mx-2">
-                    <LiaBathSolid />
-                  </span>
-                </span>
-                <span>Cầu Giấy, Hà Nội</span>
-              </div>
-
-              <div className="text-detail">
-                Bảng giá căn hộ update tháng 9/2023. - Căn studio (38m²) full
-                nội thất giá 1,8 tỷ. - Căn 2PN 1WC (51m²) full nội thất giá 2.4
-                tỷ. - Căn 2PN 1WC (61m²) full nội thất giá 2.6 tỷ. - Căn 2PN 2WC
-                (72m²) full nội thất giá 4.3 tỷ - Căn 2PN 2WC (85m²) full nội
-                thất giá 4.8 tỷ. - Căn 3PN (95m²) full nội thất giá từ 5.95 tỷ.
-                - Căn 3PN (112m²) full nội thất giá từ 6.6 tỷ - Đặc biệt, căn
-                rộng nhất dự án (122m²) full nội thất giá từ 7.8 tỷ Tham khảo
-                thêm các c...
-              </div>
-            </div>
-
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 2xl:grid-cols-2 border-2">
-              <div className=" flex  ">
+          <div className="">
+            <div className="lg:max-w-3xl">
+              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 2xl:grid-cols-2 border-2 ">
                 <img
-                  src="https://file4.batdongsan.com.vn/resize/200x200/2023/05/30/20230530095659-a050.jpg"
+                  src="https://file4.batdongsan.com.vn/crop/562x284/2023/09/28/20230928101136-0965_wm.jpg"
                   alt=""
-                  className="w-[30px] h-auto rounded-full m-4"
+                  className="w-full h-auto sm:h-full"
                 />
-                <div className=" text-sm grid items-center">
-                  <div className="pt-2 font-bold">Người Đăng</div>
-                  <span className="pb-2 text-gray-400">Đăng hôm nay</span>
+                <div className="w-auto scroll-px-0.5 ml-1">
+                  <div>
+                    <img
+                      src="https://file4.batdongsan.com.vn/crop/283x141/2023/09/28/20230928101136-ddd4_wm.jpg"
+                      alt=""
+                      className="w-full"
+                    />
+                  </div>
+                  <div className="flex pt-0.5">
+                    <img
+                      src="https://file4.batdongsan.com.vn/crop/140x140/2023/08/02/20230802142309-7dec_wm.jpg"
+                      alt=""
+                      className="w-full"
+                    />
+                    <img
+                      src="https://file4.batdongsan.com.vn/crop/140x140/2023/09/28/20230928101136-14c8_wm.jpg"
+                      alt=""
+                      className="pl-1"
+                    />
+                  </div>
                 </div>
               </div>
-              <div className="">
-                <div className="bg-teal-600 hover:bg-teal-500 flex items-center text-white m-4 p-2 rounded-lg">
-                  <span className="mx-2">
-                    <PiPhoneCallLight />
-                  </span>
-                  <span className="flex mx-2">043211234</span>
-                  <span>· Sao chép</span>
-                  <span className="ml-4">
-                    <AiOutlineHeart />
-                  </span>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="lg:max-w-xl mt-2">
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 2xl:grid-cols-2 border-2 ">
-              <img
-                src="https://file4.batdongsan.com.vn/crop/562x284/2023/09/28/20230928101136-0965_wm.jpg"
-                alt=""
-                className="w-full h-auto sm:h-full"
-              />
-              <div className="w-auto sm:w-full scroll-px-0.5 ml-1">
-                <div>
-                  <img
-                    src="https://file4.batdongsan.com.vn/crop/283x141/2023/09/28/20230928101136-ddd4_wm.jpg"
-                    alt=""
-                    className="w-full"
-                  />
-                </div>
-                <div className="flex pt-0.5">
-                  <img
-                    src="https://file4.batdongsan.com.vn/crop/140x140/2023/08/02/20230802142309-7dec_wm.jpg"
-                    alt=""
-                    className="w-full"
-                  />
-                  <img
-                    src="https://file4.batdongsan.com.vn/crop/140x140/2023/09/28/20230928101136-14c8_wm.jpg"
-                    alt=""
-                    className="pl-1"
-                  />
-                </div>
-              </div>
-            </div>
-            <div className="mx-auto border-1 p-4 shadow-inner">
-              <span className="font-bold text-sm md:text-xl">
-                BÁN CĂN STUDIO, 2 VÀ 3 PHÒNG NGỦ ĐẦY ĐỦ NỘI THẤT GIÁ TỐT NHẤT
-                TẠI VINHOMES TRẦN DUY HƯNG
-              </span>
-              <div className="flex flex-wrap gap-0 md:gap-6">
-                <span className="text-red-500 font-bold">4,3 tỷ</span>
-                <span className="text-red-500 font-bold">· 76 m²</span>
-                <span>· 56,58 tr/m²</span>
-                <span className="flex">
-                  · 2
-                  <span className="mt-1 mx-2">
-                    <LuBed />
-                  </span>
+              <div className="mx-auto border-1 p-4 shadow-inner">
+                <span className="font-bold text-sm md:text-xl">
+                  BÁN CĂN STUDIO, 2 VÀ 3 PHÒNG NGỦ ĐẦY ĐỦ NỘI THẤT GIÁ TỐT NHẤT
+                  TẠI VINHOMES TRẦN DUY HƯNG
                 </span>
-                <span className="flex">
-                  · 2
-                  <span className="mt-1 mx-2">
-                    <LiaBathSolid />
+                <div className="flex flex-wrap gap-0 md:gap-6">
+                  <span className="text-red-500 font-bold">4,3 tỷ</span>
+                  <span className="text-red-500 font-bold">· 76 m²</span>
+                  <span>· 56,58 tr/m²</span>
+                  <span className="flex">
+                    · 2
+                    <span className="mt-1 mx-2">
+                      <LuBed />
+                    </span>
                   </span>
-                </span>
-                <span>Cầu Giấy, Hà Nội</span>
+                  <span className="flex">
+                    · 2
+                    <span className="mt-1 mx-2">
+                      <LiaBathSolid />
+                    </span>
+                  </span>
+                  <span>Cầu Giấy, Hà Nội</span>
+                </div>
+
+                <div className="text-detail">
+                  Bảng giá căn hộ update tháng 9/2023. - Căn studio (38m²) full
+                  nội thất giá 1,8 tỷ. - Căn 2PN 1WC (51m²) full nội thất giá
+                  2.4 tỷ. - Căn 2PN 1WC (61m²) full nội thất giá 2.6 tỷ. - Căn
+                  2PN 2WC (72m²) full nội thất giá 4.3 tỷ - Căn 2PN 2WC (85m²)
+                  full nội thất giá 4.8 tỷ. - Căn 3PN (95m²) full nội thất giá
+                  từ 5.95 tỷ. - Căn 3PN (112m²) full nội thất giá từ 6.6 tỷ -
+                  Đặc biệt, căn rộng nhất dự án (122m²) full nội thất giá từ 7.8
+                  tỷ Tham khảo thêm các c...
+                </div>
               </div>
-              <div className="text-detail">
-                Bảng giá căn hộ update tháng 9/2023. - Căn studio (38m²) full
-                nội thất giá 1,8 tỷ. - Căn 2PN 1WC (51m²) full nội thất giá 2.4
-                tỷ. - Căn 2PN 1WC (61m²) full nội thất giá 2.6 tỷ. - Căn 2PN 2WC
-                (72m²) full nội thất giá 4.3 tỷ - Căn 2PN 2WC (85m²) full nội
-                thất giá 4.8 tỷ. - Căn 3PN (95m²) full nội thất giá từ 5.95 tỷ.
-                - Căn 3PN (112m²) full nội thất giá từ 6.6 tỷ - Đặc biệt, căn
-                rộng nhất dự án (122m²) full nội thất giá từ 7.8 tỷ Tham khảo
-                thêm các c...
+
+              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 2xl:grid-cols-2 border-2">
+                <div className=" flex  ">
+                  <img
+                    src="https://file4.batdongsan.com.vn/resize/200x200/2023/05/30/20230530095659-a050.jpg"
+                    alt=""
+                    className="w-[30px] h-auto rounded-full m-4"
+                  />
+                  <div className=" text-sm grid items-center">
+                    <div className="pt-2 font-bold">Người Đăng</div>
+                    <span className="pb-2 text-gray-400">Đăng hôm nay</span>
+                  </div>
+                </div>
+                <div className="">
+                  <div className="bg-teal-600 hover:bg-teal-500 flex items-center text-white m-4 p-2 rounded-lg">
+                    <span className="mx-2">
+                      <PiPhoneCallLight />
+                    </span>
+                    <span className="flex mx-2">043211234</span>
+                    <span>· Sao chép</span>
+                    <span className="ml-4">
+                      <AiOutlineHeart />
+                    </span>
+                  </div>
+                </div>
               </div>
             </div>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 2xl:grid-cols-2 border-2">
-              <div className=" flex  ">
+            {/* Project */}
+            <div className="lg:max-w-3xl mt-2">
+              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 2xl:grid-cols-2 border-2 ">
                 <img
-                  src="https://file4.batdongsan.com.vn/resize/200x200/2023/05/30/20230530095659-a050.jpg"
+                  src="https://file4.batdongsan.com.vn/crop/562x284/2023/09/28/20230928101136-0965_wm.jpg"
                   alt=""
-                  className="w-[30px] h-auto rounded-full m-4"
+                  className="w-full h-auto sm:h-full"
                 />
-                <div className=" text-sm grid items-center">
-                  <div className="pt-2 font-bold">Người Đăng</div>
-                  <span className="pb-2 text-gray-400">Đăng hôm nay</span>
+                <div className="w-auto sm:w-full scroll-px-0.5 ml-1">
+                  <div>
+                    <img
+                      src="https://file4.batdongsan.com.vn/crop/283x141/2023/09/28/20230928101136-ddd4_wm.jpg"
+                      alt=""
+                      className="w-full"
+                    />
+                  </div>
+                  <div className="flex pt-0.5">
+                    <img
+                      src="https://file4.batdongsan.com.vn/crop/140x140/2023/08/02/20230802142309-7dec_wm.jpg"
+                      alt=""
+                      className="w-full"
+                    />
+                    <img
+                      src="https://file4.batdongsan.com.vn/crop/140x140/2023/09/28/20230928101136-14c8_wm.jpg"
+                      alt=""
+                      className="pl-1"
+                    />
+                  </div>
                 </div>
               </div>
-              <div className="">
-                <div className="bg-teal-600 hover:bg-teal-500 flex items-center text-white m-4 p-2 rounded-lg">
-                  <span className="mx-2">
-                    <PiPhoneCallLight />
+              <div className="mx-auto border-1 p-4 shadow-inner">
+                <span className="font-bold text-sm md:text-xl">
+                  BÁN CĂN STUDIO, 2 VÀ 3 PHÒNG NGỦ ĐẦY ĐỦ NỘI THẤT GIÁ TỐT NHẤT
+                  TẠI VINHOMES TRẦN DUY HƯNG
+                </span>
+                <div className="flex flex-wrap gap-0 md:gap-6">
+                  <span className="text-red-500 font-bold">4,3 tỷ</span>
+                  <span className="text-red-500 font-bold">· 76 m²</span>
+                  <span>· 56,58 tr/m²</span>
+                  <span className="flex">
+                    · 2
+                    <span className="mt-1 mx-2">
+                      <LuBed />
+                    </span>
                   </span>
-                  <span className="flex mx-2">043211234</span>
-                  <span>· Sao chép</span>
-                  <span className="ml-4">
-                    <AiOutlineHeart />
+                  <span className="flex">
+                    · 2
+                    <span className="mt-1 mx-2">
+                      <LiaBathSolid />
+                    </span>
                   </span>
+                  <span>Cầu Giấy, Hà Nội</span>
+                </div>
+                <div className="text-detail">
+                  Bảng giá căn hộ update tháng 9/2023. - Căn studio (38m²) full
+                  nội thất giá 1,8 tỷ. - Căn 2PN 1WC (51m²) full nội thất giá
+                  2.4 tỷ. - Căn 2PN 1WC (61m²) full nội thất giá 2.6 tỷ. - Căn
+                  2PN 2WC (72m²) full nội thất giá 4.3 tỷ - Căn 2PN 2WC (85m²)
+                  full nội thất giá 4.8 tỷ. - Căn 3PN (95m²) full nội thất giá
+                  từ 5.95 tỷ. - Căn 3PN (112m²) full nội thất giá từ 6.6 tỷ -
+                  Đặc biệt, căn rộng nhất dự án (122m²) full nội thất giá từ 7.8
+                  tỷ Tham khảo thêm các c...
+                </div>
+              </div>
+              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 2xl:grid-cols-2 border-2">
+                <div className=" flex  ">
+                  <img
+                    src="https://file4.batdongsan.com.vn/resize/200x200/2023/05/30/20230530095659-a050.jpg"
+                    alt=""
+                    className="w-[30px] h-auto rounded-full m-4"
+                  />
+                  <div className=" text-sm grid items-center">
+                    <div className="pt-2 font-bold">Người Đăng</div>
+                    <span className="pb-2 text-gray-400">Đăng hôm nay</span>
+                  </div>
+                </div>
+                <div className="">
+                  <div className="bg-teal-600 hover:bg-teal-500 flex items-center text-white m-4 p-2 rounded-lg">
+                    <span className="mx-2">
+                      <PiPhoneCallLight />
+                    </span>
+                    <span className="flex mx-2">043211234</span>
+                    <span>· Sao chép</span>
+                    <span className="ml-4">
+                      <AiOutlineHeart />
+                    </span>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
 
-        <div className="mr-1">
+        <div className="mr-1 ml-4">
           <div className="hidden lg:block border border-solid md:px-8  mx-4 bg-[#F2F2F2] rounded-lg">
             <h2 className="font-bold text-sm mt-2">Mua bán nhà đất</h2>
             <div className="my-4 grid gap-3 ">
