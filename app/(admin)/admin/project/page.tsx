61087f13bfd8ae29ad49bbb9d75715a9d88c31a3
ngovanvu203 <128632824+ngovanvu2003@users.noreply.github.com>
2023-11-01 14:59:38 +0700

    fix: create project

--- app/(admin)/admin/project/page.tsx
+++ app/(admin)/admin/project/page.tsx
@@ -1,125 +1,143 @@
-"use client"
+"use client";
 import { useEffect, useState } from "react";
 import {
-    Table,
-    TableBody,
-    TableCaption,
-    TableCell,
-    TableHead,
-    TableHeader,
-    TableRow,
-} from "@/components/ui/table"
-import { Button } from "@/components/ui/button"
-import Link from "next/link"
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 import useSWR from "swr";
 import Swal from "sweetalert2";
 import { removeProject } from "@/app/api/project";
 import Image from "next/image";
 
-
 export default function TableDemo() {
-    const fetcher = (args: string) => fetch(args).then(res => res.json());
-    const { data, error, isLoading } = useSWR<any, Error, string>(`${process.env.NEXT_PUBLIC_BDS_API}/projects`, fetcher)
-    const [projects, setprojects] = useState([]);
+  const fetcher = (args: string) => fetch(args).then((res) => res.json());
+  const { data, error, isLoading } = useSWR<any, Error, string>(
+    `${process.env.NEXT_PUBLIC_BDS_API}/projects`,
+    fetcher
+  );
+  const [projects, setprojects] = useState([]);
 
-    
-    const ListAllProject = data?.response?.data;
-    
-    useEffect(() => {
-        setprojects(ListAllProject)
-      }, [ListAllProject])
+  const ListAllProject = data?.response?.data;
 
-    if (isLoading) return <div>Loading...</div>
-    if (error) return <div>error</div>
-    const HandleRemove = async (id: string | number) => {
-        try {
-            Swal.fire({
-                title: "Xóa đê",
-                showCancelButton: true,
-                confirmButtonText: "ok",
-            }).then(async (result) => {
-                if (result.isConfirmed) {
-                    await removeProject(id)
-                        .then(({ message }) => {
-                            const updatedProjects = projects.filter((item: any) => item._id !== id);
-                        
-                            // Update the state with the new data after removing the project
-                            setprojects(updatedProjects);
-                            Swal.fire({
-                                position: "top",
-                                icon: "success",
-                                title: `${message}`,
-                                showConfirmButton: false,
-                                timer: 1500,
-                            });
-                        })
-                        .catch((error) => {
-                            Swal.fire({
-                                position: "top",
-                                icon: "error",
-                                title: `${error.message}`,
-                                showConfirmButton: false,
-                                timer: 1500,
-                            });
-                        });
-                }
+  useEffect(() => {
+    setprojects(ListAllProject);
+  }, [ListAllProject]);
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>error</div>;
+  const HandleRemove = async (id: string | number) => {
+    try {
+      Swal.fire({
+        title: "Xóa đê",
+        showCancelButton: true,
+        confirmButtonText: "ok",
+      }).then(async (result) => {
+        if (result.isConfirmed) {
+          await removeProject(id)
+            .then(({ message }) => {
+              const updatedProjects = projects.filter(
+                (item: any) => item._id !== id
+              );
+
+              // Update the state with the new data after removing the project
+              setprojects(updatedProjects);
+              Swal.fire({
+                position: "top",
+                icon: "success",
+                title: `${message}`,
+                showConfirmButton: false,
+                timer: 1500,
+              });
+            })
+            .catch((error) => {
+              Swal.fire({
+                position: "top",
+                icon: "error",
+                title: `${error.message}`,
+                showConfirmButton: false,
+                timer: 1500,
+              });
             });
-        } catch (error) {
-
         }
-    };
-    return (
-        <div>
-            <div className="flex justify-between my-10">
-                <p className="text-2xl font-semibold">
-                    Danh sách dự án
-                </p>
-                <Button className='' variant="outline" ><Link href={'/admin/project/add'}>Thêm dự án</Link></Button>
-            </div>
-            <Table className="">
-                <TableHeader>
-                    <TableRow>
-                        <TableHead className="w-[100px]">#</TableHead>
-                        <TableHead>Tên dự án</TableHead>
-                        <TableHead>Gia tiền</TableHead>
-                        <TableHead className="text-right">Số phòng</TableHead>
-                        <TableHead className="text-right">Ảnh</TableHead>
-                        <TableHead className="text-right">Địa điểm</TableHead>
-                        <TableHead className="text-right">Mô tả</TableHead>
-                        <TableHead className="text-right">Địa chỉ cụ thể</TableHead>
-                        <TableHead className="text-right">Diện tích</TableHead>
-                        <TableHead className="text-right">Action</TableHead>
-                    </TableRow>
-                </TableHeader>
+      });
+    } catch (error) {}
+  };
+  return (
+    <div>
+      <div className="flex justify-between my-10">
+        <p className="text-2xl font-semibold">Danh sách dự án</p>
+        <Button className="" variant="outline">
+          <Link href={"/admin/project/add"}>Thêm dự án</Link>
+        </Button>
+      </div>
+      <Table className="">
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">#</TableHead>
+            <TableHead>Tên dự án</TableHead>
+            <TableHead>Gia tiền</TableHead>
+            <TableHead className="text-right">Số phòng</TableHead>
+            <TableHead className="text-right">Ảnh</TableHead>
+            <TableHead className="text-right">Địa điểm</TableHead>
+            <TableHead className="text-right">Mô tả</TableHead>
+            <TableHead className="text-right">Địa chỉ cụ thể</TableHead>
+            <TableHead className="text-right">Diện tích</TableHead>
+            <TableHead className="text-right">Action</TableHead>
+          </TableRow>
+        </TableHeader>
 
-                <TableBody className=" border-collapse border">
-                    {ListAllProject?.map((invoice: any, index: number) => (
-                        <TableRow key={invoice?._id}>
-                            <TableCell className="font-medium">{index + 1}</TableCell>
-                            <TableCell className="font-medium">{invoice?.project_name}</TableCell>
-                            <TableCell>{invoice?.project_price}</TableCell>
-                            <TableCell>  {invoice?.project_room}</TableCell>
-                            <TableCell>
-                                <Image
-                                    className="w-[50px]"
-                                    src={invoice?.project_image}
-                                    alt="anh phong"
-                                    width={300}
-                                    height={300}
-                                />  </TableCell>
-                            <TableCell>{invoice?.project_location}</TableCell>
-                            <TableCell>{invoice?.project_content}</TableCell>
-                            <TableCell className="text-right">{invoice?.project_address}</TableCell>
-                            <TableCell className="text-right">{invoice?.project_acreage}</TableCell>
-                            <TableCell className="text-right">{invoice?.project_acreage}</TableCell>
-                            <TableCell className="text-right">
-                                <Button className='mr-2' variant="outline"><Link href={`/admin/category/update/`}>Cập nhật</Link></Button>
-                                <Button className='' variant="outline" onClick={() => HandleRemove(invoice?._id)}>Xóa</Button>
-                            </TableCell>
-                        </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </div>
-    )
+        <TableBody className=" border-collapse border">
+          {ListAllProject?.map((invoice: any, index: number) => (
+            <TableRow key={invoice?._id}>
+              <TableCell className="font-medium">{index + 1}</TableCell>
+              <TableCell className="font-medium">
+                {invoice?.project_name}
+              </TableCell>
+              <TableCell>{invoice?.project_price}</TableCell>
+              <TableCell> {invoice?.project_room}</TableCell>
+              <TableCell>
+                <Image
+                  className="w-[50px]"
+                  src={invoice?.project_image}
+                  alt="anh phong"
+                  width={300}
+                  height={300}
+                />{" "}
+              </TableCell>
+              <TableCell>{invoice?.project_location}</TableCell>
+              <TableCell>{invoice?.project_content}</TableCell>
+              <TableCell className="text-right">
+                {invoice?.project_address}
+              </TableCell>
+              <TableCell className="text-right">
+                {invoice?.project_acreage}
+              </TableCell>
+              <TableCell className="text-right">
+                {invoice?.project_acreage}
+              </TableCell>
+              <TableCell className="text-right">
+                <Button className="mr-2" variant="outline">
+                  <Link href={`/admin/category/update/`}>Cập nhật</Link>
+                </Button>
+                <Button
+                  className=""
+                  variant="outline"
+                  onClick={() => HandleRemove(invoice?._id)}
+                >
+                  Xóa
+                </Button>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
+  );
 }
--- app/api/project.ts
+++ app/api/project.ts
@@ -2,60 +2,56 @@ import useSWR from "swr";
 import axios from "axios";
 import useGetToken from "./getToken";
 
-
 const apiUrl = `${process.env.NEXT_PUBLIC_BDS_API}/projects`;
 
 const fetcher = (url: any) => fetch(url).then((res) => res.json());
 export const fetchUser = (id: string) => {
-    const url = `/api/projects/${id}`;
-    return useSWR(url, fetcher);
+  const url = `/api/projects/${id}`;
+  return useSWR(url, fetcher);
 };
 
 export const createProject = async (data: any) => {
-    const token = useGetToken();
-    const url = `${apiUrl}`;
-    return await fetch(url, {
-        method: 'POST',
-        body: JSON.stringify(data),
-
-        headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${token}`
-        },
-    }).then((res) => res.json());
-}
+  const token = useGetToken();
+  const url = `${apiUrl}`;
+  return await fetch(url, {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  }).then((res) => res.json());
+};
 export const removeProject = async (id: string | number) => {
-    const url = `${apiUrl}/` + id;
-    return await fetch(url, {
-        method: 'DELETE',
-    }).then((res) => res.json());
-}
+  const url = `${apiUrl}/` + id;
+  return await fetch(url, {
+    method: "DELETE",
+  }).then((res) => res.json());
+};
 
 export const upLoadFiles = async (files: any) => {
-    if (files) {
-        const CLOUD_NAME = `${process.env.NEXT_PUBLIC_CLOUD_NAME}`;
-        const PRESET_NAME = `${process.env.NEXT_PUBLIC_PRESET_NAME}`;
-        const FOLDER_NAME = "portfolio";
-        const urls = [];
-        const api = `https://api.cloudinary.com/v1_1/${CLOUD_NAME}/image/upload`;
+  if (files) {
+    const CLOUD_NAME = `${process.env.NEXT_PUBLIC_CLOUD_NAME}`;
+    const PRESET_NAME = `${process.env.NEXT_PUBLIC_PRESET_NAME}`;
+    const FOLDER_NAME = "portfolio";
+    const urls = [];
+    const api = `https://api.cloudinary.com/v1_1/${CLOUD_NAME}/image/upload`;
 
-        const formDataFL = new FormData(); // key : value
+    const formDataFL = new FormData(); // key : value
 
-        formDataFL.append("upload_preset", PRESET_NAME);
-        formDataFL.append("folder", FOLDER_NAME);
+    formDataFL.append("upload_preset", PRESET_NAME);
+    formDataFL.append("folder", FOLDER_NAME);
 
-        for (const file of files) {
-            formDataFL.append("file", file)
-            const response = await axios.post(api, formDataFL, {
-                headers: {
-                    "Content-Type": "multipart/form-data",
-                },
-            })
-            urls.push(response?.data.secure_url)
-        };
-        console.log(urls)
-        return urls
+    for (const file of files) {
+      formDataFL.append("file", file);
+      const response = await axios.post(api, formDataFL, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+      urls.push(response?.data.secure_url);
     }
-
-}
-
+    console.log(urls);
+    return urls;
+  }
+};
