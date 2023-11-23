6ca35cf79b331b37d265949b1954efa5d90c51b4
ngovanvu203 <128632824+ngovanvu2003@users.noreply.github.com>
2023-11-23 15:40:18 +0700

    add react query

---
+++ utils/Provider.tsx
@@ -0,0 +1,19 @@
+"use client";
+import React from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { ReactQueryStreamedHydration } from "@tanstack/react-query-next-experimental";
+// npm install @tanstack/react-query-next-experimental
+
+function Providers({ children }: React.PropsWithChildren) {
+  const [queryClient] = React.useState(new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryStreamedHydration>{children}</ReactQueryStreamedHydration>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}
+
+export default Providers;
